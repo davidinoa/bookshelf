b50e2eb1349f1afc7ea38ff3865ef31bfbd39160
David Inoa <davidinoa@gmail.com>
2024-08-30 19:59:02 -0400

    feat: add auth modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
+'use client'
+
+import { useState } from 'react'
+import { Dialog } from '@reach/dialog'
 import Logo from '@/components/logo'
+import '@reach/dialog/styles.css'
+
+type ModalState = 'login' | 'register' | 'none'
 
 export default function Home() {
+  const [openModal, setOpenModal] = useState<ModalState>('none')
   return (
     <div>
       <Logo width={80} height={80} />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('Login clicked')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('Register clicked')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Register form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </div>
   )
 }
