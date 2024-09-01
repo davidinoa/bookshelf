aecccbcbb06ac57214d8b2d5a71ffe1dbd7e7ea4
David Inoa <davidinoa@gmail.com>
2024-09-01 15:12:16 -0400

    chore(tailwind): add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        text: '#434449',
+        gray: '#f1f2f7',
+        gray10: '#f1f1f4',
+        gray20: '#e4e5e9',
+        gray80: '#6f7077',
+        indigo: '#3f51b5',
+        indigoDarken10: '#364495',
+        indigoLighten80: '#b7c1f8',
+        yellow: '#ffc107',
+        green: '#4caf50',
+        danger: '#ef5350',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
       fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
